c45d75e7294c49265d13666f73ef5e782f372e15
ilyaelagin <motortsi@gmail.com>
2021-06-25 14:15:28 +0300

    refactor sql 2

--- full.sql
+++ full.sql
@@ -2,39 +2,42 @@ CREATE SCHEMA hostel;
 
 DROP SCHEMA hostel;
 
+CREATE TABLE categories
+(
+    category    VARCHAR2(20) PRIMARY KEY,
+    description TEXT
+);
+
+CREATE TABLE apartments
+(
+    number        INTEGER PRIMARY KEY,
+    rooms         INTEGER,
+    cleaning_date TIMESTAMP,
+    category      VARCHAR2(20) REFERENCES categories (category)
+);
+
 CREATE TABLE guests
 (
-    id IDENTITY PRIMARY KEY,
-    name VARCHAR2(50) NOT NULL,
-    surname VARCHAR2(50) NOT NULL,
-    passport VARCHAR2(50) UNIQUE,
-    foto BYTEA,
-    birth DATE,
-    check_in DATE,
+    id        IDENTITY PRIMARY KEY,
+    name      VARCHAR2(50) NOT NULL,
+    surname   VARCHAR2(50) NOT NULL,
+    passport  VARCHAR2(50) UNIQUE,
+    foto      BYTEA,
+    birth     DATE,
+    check_in  DATE,
     check_out DATE,
-    apartment INTEGER REFERENCES apartments(number)
+    apartment INTEGER REFERENCES apartments (number)
 );
 
-CREATE TABLE apartments (
-    number INTEGER PRIMARY KEY,
-    rooms INTEGER,
-    cleaning_date TIMESTAMP,
-    category VARCHAR2(20) REFERENCES categories(category)
-);
-
-CREATE TABLE categories (
-     category VARCHAR2(20) PRIMARY KEY,
-     description TEXT
-);
-
-CREATE TABLE users (
-    id IDENTITY PRIMARY KEY,
-    name VARCHAR2(20) NOT NULL,
-    role VARCHAR2(20) NOT NULL REFERENCES roles(role)
-);
-
-CREATE TABLE roles (
+CREATE TABLE roles
+(
     role VARCHAR2(20) PRIMARY KEY
 );
 
-DROP TABLE categories;
+CREATE TABLE users
+(
+    id   IDENTITY PRIMARY KEY,
+    name VARCHAR2(20) NOT NULL,
+    role VARCHAR2(20) NOT NULL REFERENCES roles (role)
+
+);
